0e261104511df34db70dabbf57cac2057a034ccd
molottva <molottva@list.ru>
2022-06-03 21:42:57 +0300

    CI failed test

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void shouldPostRawTextTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Это что, API автотест?")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(512)
+                .body("data", equalTo("Это что, API автотест?"));
+    }
+}
